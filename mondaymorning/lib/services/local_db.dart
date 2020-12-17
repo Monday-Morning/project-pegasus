45db4399424c33550e247cc85b7da7b0270b1b9a
Sriram Patibanda <patibandasriram2k02@gmail.com>
2020-12-17 21:13:19 +0530

    Adds documentation for LocalDb

--- mondaymorning/lib/services/local_db.dart
+++ mondaymorning/lib/services/local_db.dart
@@ -1,23 +1,33 @@
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
+/// Contains the box names that will be used for LocalDb
 enum LocalDbBoxes {
+  /// User data box for user data like 'id', 'name', 'email', etc.
   userData,
 }
 
+/// A service dedicated to store the local app data in [Hive].
+/// Use `LocalDb.getValue()` for retrieving the data from the database.
+/// Use `LocalDb.setValue()` to write or update the data in the database.
+/// Use `LocalDb.deleteValue()` to delete data in the database.
 class LocalDb {
+  /// Initialises the Hive Database
   Future initiateLocalDb() async {
     await Hive.initFlutter();
   }
 
+  /// Opens the box named box
   Future<void> openBox(String box) async {
     await Hive.openBox(box);
   }
 
+  /// Closes the box named box
   Future<void> closeBox(String box) async {
     await Hive.box(box).close();
   }
 
+  /// Returns the value for the key from a box named boxName
   dynamic getValue(String boxName, String key) {
     assert(Hive.isBoxOpen(boxName));
 
@@ -26,6 +36,7 @@ class LocalDb {
     return box.get(key);
   }
 
+  /// Inserts a key-value pair into the box named boxname
   dynamic setValue(String boxName, String key, String value) {
     assert(Hive.isBoxOpen(boxName));
 
@@ -34,6 +45,7 @@ class LocalDb {
     box.put(key, value);
   }
 
+  /// Deletes the key-value pair from the box named boxName
   dynamic deleteValue(String boxName, String key) {
     assert(Hive.isBoxOpen(boxName));
 
