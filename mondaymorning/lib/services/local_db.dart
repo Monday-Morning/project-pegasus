dc44c3d1d5101907388d6b8d22258bfdb8432f6d
SriramPatibanda <56649197+SriramPatibanda@users.noreply.github.com>
2020-12-21 17:01:18 +0530

    Setup Local Database Using Hive (#5)
    * localDb setup using HiveDb for CRUD functions along with tests
    
    * Adds documentation for LocalDb
    
    * Mock Hive in LocalDb tests, test getValue, use final declarations
    
    Mock the instances of Hive in tests, use final declarations for boxes in LocalDb service write test for getValue
    
    * change type of deleteValue() and setValue() functions to void, documented pubspec.yaml
    
    * Add spaces in comments in pubspec.yaml
    
    Co-authored-by: Smarak Das <34758667+Thesmader@users.noreply.github.com>
    
    Co-authored-by: Smarak Das <34758667+Thesmader@users.noreply.github.com>

---
+++ mondaymorning/lib/services/local_db.dart
@@ -0,0 +1,63 @@
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+/// Contains the box names that will be used for LocalDb
+enum LocalDbBoxes {
+  /// User data box for user data like 'id', 'name', 'email', etc.
+  userData,
+}
+
+/// A service dedicated to store the local app data in [Hive].
+/// Use `LocalDb.getValue()` for retrieving the data from the database.
+/// Use `LocalDb.setValue()` to write or update the data in the database.
+/// Use `LocalDb.deleteValue()` to delete data in the database.
+class LocalDb {
+  HiveInterface hiveInterface;
+  LocalDb({this.hiveInterface}) {
+    if (hiveInterface == null) {
+      this.hiveInterface = Hive;
+    }
+  }
+
+  /// Initialises the Hive Database
+  Future initiateLocalDb() async {
+    await hiveInterface.initFlutter();
+  }
+
+  /// Opens the box named box
+  Future<void> openBox(String box) async {
+    await hiveInterface.openBox(box);
+  }
+
+  /// Closes the box named box
+  Future<void> closeBox(String box) async {
+    await hiveInterface.box(box).close();
+  }
+
+  /// Returns the value for the key from a box named boxName
+  dynamic getValue(String boxName, String key) {
+    assert(hiveInterface.isBoxOpen(boxName));
+
+    final box = hiveInterface.box(boxName);
+
+    return box.get(key);
+  }
+
+  /// Inserts a key-value pair into the box named boxname
+  void setValue(String boxName, String key, String value) {
+    assert(hiveInterface.isBoxOpen(boxName));
+
+    final box = hiveInterface.box(boxName);
+
+    box.put(key, value);
+  }
+
+  /// Deletes the key-value pair from the box named boxName
+  void deleteValue(String boxName, String key) {
+    assert(hiveInterface.isBoxOpen(boxName));
+
+    final box = hiveInterface.box(boxName);
+
+    box.delete(key);
+  }
+}
