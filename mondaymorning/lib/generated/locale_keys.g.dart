60421ecbc72247adbcc4604421f49477918e11bf
ajibeshMohapatra <62394580+ajibeshMohapatra@users.noreply.github.com>
2020-12-29 22:49:39 +0530

    setup localization
    using easy_localization package generated code for string literals and used them in the app.

---
+++ mondaymorning/lib/generated/locale_keys.g.dart
@@ -0,0 +1,6 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+// ignore_for_file: public_member_api_docs
+abstract class LocaleKeys {
+  static const appName = 'appName';
+  static const title = 'title';
+}
--- mondaymorning/lib/ui/views/home/home_viewmodel.dart
+++ mondaymorning/lib/ui/views/home/home_viewmodel.dart
@@ -1,8 +1,10 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:mondaymorning/generated/locale_keys.g.dart';
 import 'package:stacked/stacked.dart';
 
 /// ViewModel for [HomeView]
 class HomeViewmodel extends BaseViewModel {
-  final String _title = "Welcome to Monday Morning App";
+  final String _title = LocaleKeys.title.tr();
   String get title => _title;
 
   int _counter = 0;
