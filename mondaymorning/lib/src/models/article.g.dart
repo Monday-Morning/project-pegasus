fb3b6f0f008e4cfc5d07e6cf879fdfe44e603bb2
Sriram Patibanda <patibandasriram2k02@gmail.com>
2021-10-07 17:10:28 +0530

    Create Article Data Model

---
+++ mondaymorning/lib/src/models/article.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'article.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Article _$_$_ArticleFromJson(Map<String, dynamic> json) {
+  return _$_Article(
+    id: json['id'] as String,
+    title: json['title'] as String,
+    inshort: json['inshort'] as String,
+  );
+}
+
+Map<String, dynamic> _$_$_ArticleToJson(_$_Article instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'inshort': instance.inshort,
+    };
