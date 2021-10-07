332caa818228479c33c59d6fd2db8c64654f7b6a
Sriram Patibanda <patibandasriram2k02@gmail.com>
2021-10-07 14:31:23 +0530

    Change folder structure according to best practices

--- mondaymorning/lib/main.dart
+++ mondaymorning/lib/main.dart
@@ -1,26 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:mondaymorning/core/navigation_service.dart';
-import 'package:mondaymorning/core/router.dart';
-import 'package:mondaymorning/ui/home_view.dart';
+import 'package:mondaymorning/src/app.dart';
 
 void main() {
   runApp(const ProviderScope(child: MMApp()));
-}
-
-/// The main app.
-class MMApp extends StatelessWidget {
-  const MMApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MondayMorning',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(),
-      home: const HomeView(),
-      navigatorKey: context.read(navigatorProvider).navigatorKey,
-      onGenerateRoute: AppRouter().generateRoute,
-    );
-  }
 }
