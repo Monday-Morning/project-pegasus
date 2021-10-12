cd465184f099f2c2fc6b8cee64d780f53c635f0d
Sriram Patibanda <patibandasriram2k02@gmail.com>
2021-10-12 15:58:36 +0530

    Change folder structure of the app

--- mondaymorning/lib/src/views/home_view.dart
+++ mondaymorning/lib/src/views/home_view.dart
@@ -1,8 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:mondaymorning/src/core/router.gr.dart';
 import 'package:mondaymorning/src/providers/providers.dart';
+import 'package:mondaymorning/src/services/navigation/router.gr.dart';
 
 /// Home View widget of the app.
 class HomeView extends ConsumerWidget {
