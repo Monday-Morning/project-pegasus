d715471a0ad3beebe118cbf667c99beceec96a33
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2022-11-06 00:19:20 +0530

    feat: init base app
    Co-authored-by: Asutosh Ranjan <asutoshranjan.work@gmail.com>
    Co-authored-by: Sambit Sankalp <sambit.sankalp.official@gmail.com>

---
+++ app/lib/src/app.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mondaymorning/src/pages/full_error_page.dart';
+import 'package:mondaymorning/src/pages/full_loading_page.dart';
+import 'package:mondaymorning/src/services/router/mm_router.gr.dart';
+import 'package:mondaymorning/src/services/themes/index.dart';
+// import 'package:mondaymorning/src/services/themes/size_config.dart';
+import 'package:mondaymorning/src/store/states/app_config/app_config_provider.dart';
+import 'package:mondaymorning/src/store/states/app_config/app_config_type.dart';
+
+class ProjectPegasus extends ConsumerWidget {
+  ProjectPegasus({Key? key}) : super(key: key);
+
+  final mmRouter = MMRouter();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return FutureBuilder<AppConfig>(
+      future: ref.watch(appConfigProvider.future),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          if (kDebugMode) {
+            print(snapshot.error);
+          }
+          return FullErrorPage();
+        }
+        if (snapshot.hasData) {
+          return MaterialApp.router(
+            title: 'MondayMorning',
+            themeMode: snapshot.data!.themeMode,
+            // themeMode: ThemeMode.dark,
+            theme: MMTheme.materialLight,
+            darkTheme: MMTheme.materialDark,
+            debugShowCheckedModeBanner: false,
+            routeInformationParser: mmRouter.defaultRouteParser(),
+            routerDelegate: mmRouter.delegate(),
+          );
+        }
+        return FullLoadingPage();
+      },
+    );
+  }
+}
