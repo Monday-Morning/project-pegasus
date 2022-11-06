12bf9903c1b1f9fe331866742b14bb1987c60789
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2022-11-06 18:36:43 +0530

    refactor: use single switchable repo provider

--- app/lib/src/api/repositories/issue/issue_repository_provider.dart
+++ app/lib/src/api/repositories/issue/issue_repository_provider.dart
@@ -1,10 +1,11 @@
 import 'package:mondaymorning/src/api/repositories/issue/graph_issue_repository.dart';
+import 'package:mondaymorning/src/api/repositories/issue/issue_repository.dart';
 import 'package:mondaymorning/src/services/graphql/graphql_service.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-part 'graph_issue_repository_provider.g.dart';
+part 'issue_repository_provider.g.dart';
 
 @riverpod
-GraphIssueRepository graphIssueRepository(GraphIssueRepositoryRef ref) {
+IssueRepository issueRepository(IssueRepositoryRef ref) {
   return GraphIssueRepository(graphClient: GraphQLService());
 }
