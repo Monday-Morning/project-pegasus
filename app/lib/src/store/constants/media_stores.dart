8b2bea9e691a4d3b8c925143173418c50704a6f7
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2022-11-07 17:48:58 +0530

    fix: dart errors

--- app/lib/src/store/constants/media_stores.dart
+++ app/lib/src/store/constants/media_stores.dart
@@ -1,14 +1,12 @@
 class MediaStores {
-  static const String ADAMANTIUM_ARCHIVE_A =
-      'https://ik.imagekit.io/adamantiumA';
-  static const String ADAMANTIUM_ARCHIVE_B =
-      'https://ik.imagekit.io/adamantiumB';
-  static const String ACTIVE = 'https://ik.imagekit.io/infinityA';
+  static const String adamantiumArchiveA = 'https://ik.imagekit.io/adamantiumA';
+  static const String adamantiumArchiveB = 'https://ik.imagekit.io/adamantiumB';
+  static const String active = 'https://ik.imagekit.io/infinityA';
 
   static const Map<String, String> stores = {
-    'ADAMANTIUM_ARCHIVE_A': ADAMANTIUM_ARCHIVE_A,
-    'ADAMANTIUM_ARCHIVE_B': ADAMANTIUM_ARCHIVE_B,
-    'ACTIVE': ACTIVE,
+    'ADAMANTIUM_ARCHIVE_A': adamantiumArchiveA,
+    'ADAMANTIUM_ARCHIVE_B': adamantiumArchiveB,
+    'ACTIVE': active,
   };
 
   MediaStores._();
