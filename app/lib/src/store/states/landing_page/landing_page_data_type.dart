8cf82185e35a090207206094577c11dd8a460aa4
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2022-11-13 13:11:21 +0530

    refactor: change home to landing page (#41)

--- app/lib/src/store/states/landing_page/landing_page_data_type.dart
+++ app/lib/src/store/states/landing_page/landing_page_data_type.dart
@@ -1,12 +1,12 @@
 import 'package:mondaymorning/src/api/models/issue/issue.dart';
 import 'package:mondaymorning/src/api/models/squiggle/squiggle.dart';
 
-class HomePageData {
+class LandingPageData {
   final Issue latestIssue;
   // final List<Issue>? issues;
   final Squiggle latestSquiggle;
 
-  HomePageData({
+  LandingPageData({
     required this.latestIssue,
     // required this.issues,
     required this.latestSquiggle,
