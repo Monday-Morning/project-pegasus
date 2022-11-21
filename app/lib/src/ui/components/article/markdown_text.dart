f1ed18c34d26c65259d603950da49eac7d95daae
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2022-11-21 07:26:19 +0530

    feat: add override support to makrdown text

--- app/lib/src/ui/components/article/markdown_text.dart
+++ app/lib/src/ui/components/article/markdown_text.dart
@@ -5,24 +5,35 @@ class MarkdownText extends StatelessWidget {
   const MarkdownText({
     Key? key,
     required this.text,
+    this.overrideTextColor,
+    this.overrideTextScale,
   }) : super(key: key);
 
   final String text;
+  final Color? overrideTextColor;
+  final double? overrideTextScale;
 
   @override
   Widget build(BuildContext context) {
-    return Markdown(
+    return MarkdownBody(
       selectable: true,
       shrinkWrap: true,
       onTapLink: (text, href, title) {
         // TODO: add link to browser
       },
       softLineBreak: true,
-      physics: NeverScrollableScrollPhysics(),
-      padding: EdgeInsets.zero,
       data: text,
-      styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context))
-          .copyWith(textAlign: WrapAlignment.spaceBetween),
+      styleSheetTheme: MarkdownStyleSheetBaseTheme.material,
+      styleSheet: MarkdownStyleSheet.fromTheme(
+        overrideTextColor == null
+            ? Theme.of(context)
+            : Theme.of(context).copyWith(
+                textTheme: Theme.of(context).textTheme.apply(
+                      bodyColor: overrideTextColor,
+                      fontSizeFactor: overrideTextScale ?? 1,
+                    ),
+              ),
+      ),
     );
   }
 }
