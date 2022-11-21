a80868ffbe01a3055e3f9a8f0d698e66208cbb3d
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2022-11-22 00:56:56 +0530

    feat: category page

--- app/lib/src/pages/category_page.dart
+++ app/lib/src/pages/category_page.dart
@@ -1,9 +1,12 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mondaymorning/src/services/router/mm_router.gr.dart';
 import 'package:mondaymorning/src/store/constants/categories.dart';
 import 'package:mondaymorning/src/store/states/category_page/category_page_data_type.dart';
 import 'package:mondaymorning/src/store/states/category_page/category_page_provider.dart';
+import 'package:mondaymorning/src/ui/components/category/category_app_bar.dart';
 import 'package:mondaymorning/src/ui/screens/full_error_screen.dart';
 import 'package:mondaymorning/src/ui/screens/full_loading_screen.dart';
 
@@ -12,7 +15,7 @@ class CategoryPage extends ConsumerWidget {
 
   const CategoryPage({
     super.key,
-    required this.category,
+    @PathParam('category') required this.category,
   });
 
   @override
@@ -36,7 +39,32 @@ class CategoryPage extends ConsumerWidget {
         if (kDebugMode) {
           print(snapshot.data?.articles);
         }
-        return Container();
+        return AutoTabsRouter.tabBar(
+          routes: [
+            AllCategoryRoute(articles: snapshot.data!.articles),
+            ...(Categories.subCategoryIdsByCategory[category]!
+                .map(
+                  (e) => SubCategoryRoute(
+                    subCategory: Categories.allCategoryNames[e]!,
+                    subCategoryId: e,
+                    articles: snapshot.data!.articles[Categories
+                        .subCategoryIdsByCategory[category]!
+                        .indexOf(e)
+                        .abs()],
+                  ),
+                )
+                .toList()),
+          ],
+          builder: (context, child, tabController) {
+            return Scaffold(
+              appBar: CategoryAppBar(
+                category: category,
+                tabController: tabController,
+              ),
+              body: child,
+            );
+          },
+        );
       },
     );
   }
