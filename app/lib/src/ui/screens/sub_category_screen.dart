e01076b55f0f241e1947df35a24f666b11302931
Sambit Sankalp <82284130+sambit-sankalp@users.noreply.github.com>
2022-12-21 01:55:35 +0530

    feat: subcategory page made (#47)
    * feat:subcategory page made
    
    * fix:remove for loop

--- app/lib/src/ui/screens/sub_category_screen.dart
+++ app/lib/src/ui/screens/sub_category_screen.dart
@@ -1,7 +1,11 @@
-import 'package:auto_route/annotations.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:mondaymorning/src/api/models/article/article.dart';
+import 'package:mondaymorning/src/services/router/mm_router.dart';
+import 'package:mondaymorning/src/services/themes/size_config.dart';
+import 'package:mondaymorning/src/ui/components/article/article_carousel.dart';
+import 'package:mondaymorning/src/ui/components/article/small_article_card.dart';
 
 class SubCategoryScreen extends StatelessWidget {
   final String subCategory;
@@ -20,8 +24,53 @@ class SubCategoryScreen extends StatelessWidget {
     if (kDebugMode) {
       print(articles);
     }
-    return Center(
-        child:
-            Text('SubCategory ID: $subCategoryId\nSubCategory: $subCategory'));
+    final notFeaturedArticles =
+        articles.sublist((articles.length / 2).round(), articles.length);
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: SizeConfig.safeBlockHorizontal! * 2,
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    SizedBox(
+                      height: SizeConfig.safeBlockHorizontal! * 2,
+                    ),
+                    ArticleCarousel(
+                      featured: articles.sublist(
+                        0,
+                        (articles.length / 2).round(),
+                      ),
+                    ),
+                    ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: notFeaturedArticles.length,
+                      itemBuilder: (context, index) => SmallArticleCard(
+                        article: notFeaturedArticles[index],
+                        onTileTap: () {
+                          AutoRouter.of(context).push(
+                            ArticleRoute(
+                              articleId: notFeaturedArticles[index].id,
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
