6aac98ce20caf98cfaf04d33ed9ffe11a7586abb
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2023-01-10 02:09:14 +0530

    chore: clean unused code and imports

--- app/lib/src/app.dart
+++ app/lib/src/app.dart
@@ -1,10 +1,7 @@
-import 'dart:async';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mondaymorning/src/pages/full_error_page.dart';
-import 'package:mondaymorning/src/pages/full_loading_page.dart';
 import 'package:mondaymorning/src/pages/splash_page.dart';
 import 'package:mondaymorning/src/services/router/mm_router.dart';
 import 'package:mondaymorning/src/services/themes/index.dart';
@@ -19,17 +16,16 @@ class ProjectPegasus extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return FutureBuilder(
-      //future: Future.wait([ref.watch(appConfigProvider.future), Future.delayed(Duration(milliseconds: 4600))]),
+    return FutureBuilder<AppConfig>(
       future: ref.watch(appConfigProvider.future),
-      builder: (context, snapshot ) {   //AsyncSnapshot<List<dynamic>> snapshot) {
+      builder: (BuildContext context, AsyncSnapshot<AppConfig> snapshot) {
         if (snapshot.hasError) {
           if (kDebugMode) {
             print(snapshot.error);
           }
           return FullErrorPage();
         }
-        if ( snapshot.hasData ) {   //&& snapshot.connectionState == ConnectionState.done) {
+        if (snapshot.hasData) {
           return MaterialApp.router(
             title: 'MondayMorning',
             themeMode: snapshot.data!.themeMode,
