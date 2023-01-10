17f0aa9300b69b1e9b828ff96b81c6175c487a15
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2023-01-10 22:58:49 +0530

    chore: delete old app folder

--- mondaymorning/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:mondaymorning/src/app.dart';
-
-void main() {
-  runApp(ProviderScope(child: MMApp()));
-}
