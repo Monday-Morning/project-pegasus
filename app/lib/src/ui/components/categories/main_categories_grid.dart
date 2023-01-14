0c7e13fc36c8d7200c9a5ec13daaf43c57641973
Asutosh Ranjan <asutoshranjan.work@gmail.com>
2023-01-14 15:48:45 +0530

    added common assets to new folder

--- app/lib/src/ui/components/categories/main_categories_grid.dart
+++ app/lib/src/ui/components/categories/main_categories_grid.dart
@@ -18,7 +18,7 @@ class MainCategoriesGrid extends StatelessWidget {
           children: [
             BigCategoryCard(
               image:
-                  'assets/images/light/campus.svg',
+                  'assets/images/common_images/campus.svg',
               title: 'Campus',
               onCardTap: () {
                 AutoRouter.of(context).push(
@@ -28,7 +28,7 @@ class MainCategoriesGrid extends StatelessWidget {
             ),
             BigCategoryCard(
               image:
-                  'assets/images/light/connect.svg',
+                  'assets/images/common_images/connect.svg',
               title: 'Connect',
               onCardTap: () {
                 AutoRouter.of(context).push(
@@ -38,7 +38,7 @@ class MainCategoriesGrid extends StatelessWidget {
             ),
             BigCategoryCard(
               image:
-                  'assets/images/light/ddandcwc.svg',
+                  'assets/images/common_images/ddandcwc.svg',
               title: 'DD & CWC',
               onCardTap: () {
                 AutoRouter.of(context).push(
@@ -52,7 +52,7 @@ class MainCategoriesGrid extends StatelessWidget {
         Row(
           children: [
             BigCategoryCard(
-              image: 'assets/images/light/career.svg',
+              image: 'assets/images/common_images/career.svg',
               title: 'Career',
               onCardTap: () {
                 AutoRouter.of(context).push(
@@ -61,7 +61,7 @@ class MainCategoriesGrid extends StatelessWidget {
               },
             ),
             BigCategoryCard(
-              image: 'assets/images/light/alumni.svg',
+              image: 'assets/images/common_images/alumni.svg',
               title: 'Alumni',
               onCardTap: () {
                 AutoRouter.of(context).push(
@@ -70,7 +70,7 @@ class MainCategoriesGrid extends StatelessWidget {
               },
             ),
             BigCategoryCard(
-              image: 'assets/images/light/expression.svg',
+              image: 'assets/images/common_images/expression.svg',
               title: 'Expression',
               onCardTap: () {
                 AutoRouter.of(context).push(
