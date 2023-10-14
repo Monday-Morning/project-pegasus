3422ddcd237e50283794fd7dbede855ea9f0da73
Sambit Sankalp <sambit.sankalp.official@gmail.com>
2023-10-14 19:07:44 +0530

    fix: server API endpoint

--- app/lib/src/services/graphql/graphql_service.dart
+++ app/lib/src/services/graphql/graphql_service.dart
@@ -15,7 +15,7 @@ class GraphQLService implements GraphQLAPI {
   Future<void> init() async {
     final HttpLink httpLink = HttpLink(
       !kDebugMode
-          ? 'https://mondaymorning.nitrkl.ac.in/api/v1/graph'
+          ? 'https://mm.dashnet.in/api/v1/graph'
           : 'http://localhost:5000/v1/graph',
     );
 
